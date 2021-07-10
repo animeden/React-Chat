f0896d8756c5ac0a557b0914a24aa3cddfee9e18
Denis Holei <goleiden@gmail.com>
2021-07-10 13:10:01 +0300

    second commitV2

--- src/ChatForms/chatForm.js
+++ src/ChatForms/chatForm.js
@@ -1,38 +1,110 @@
 import React from 'react'
+import axios from 'axios'
 
-class ChatForm extends React.Component {
+class ChatForm extends React.Component{
+
     constructor(props) {
       super(props);
-      this.state = {value: 'chat', value2:'user'};
-  
-      this.handleChange = this.handleChange.bind(this);
+
+      this.state ={chatName: 'Chat', userName:'User'};
+
+      this.chatNameChange = this.chatNameChange.bind(this);
+      this.userNameChange = this.userNameChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
+      this.func = this.func.bind(this);
+      this.valid = this.valid.bind(this);
     }
   
-    handleChange(event) {
-      this.setState({value: event.target.value, value2: event.target.value2});
+  
+    chatNameChange(event) {
+
+      this.setState({chatName: event.target.value});
+
     }
+
+    userNameChange(event) {
+
+      this.setState({userName: event.target.value});
+
+    }
+  
   
     handleSubmit(event) {
-      alert('Chat name: ' + this.state.value + '; User name: ' + this.state.value2);
+
       event.preventDefault();
+
+      this.valid(this.state.userName, this.state.chatName);
+
+      
+
+    }
+
+    async func(client_id, name) {
+
+      await axios({
+          method: 'post',
+          url: "http://localhost:3000/createchat/api/chats/create-chat" ,
+          data: JSON.stringify({
+            client_id, name
+          }),
+          headers: {
+              'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+              'Authorization': '',
+          }
+      }).then(function (response) {
+          if (response.data !== '' && response.data.constructor === Object) {
+              console.log(response.data)     
+          }
+      }).catch(function (error) {
+          console.log(error)
+      });
+
+    } 
+
+    valid(userName, chatName){
+      let error = '';
+      let errbool = false;
+
+      if(!userName){
+        error = error + ' User name cant be empty;';
+        errbool = true;
+      }
+      if(!chatName){
+        error = error + ' Chat name cant be empty;';
+        errbool = true;
+      }
+      if(errbool){
+        alert(error);
+      }
+      if(!errbool){
+        this.func(this.state.userName, this.state.chatName);
+        alert('Chat name: ' + this.state.chatName + '; User name: ' + this.state.userName);
+      }
     }
   
-    render() {
+    render(){
       return (
         <form onSubmit={this.handleSubmit}>
-          <label>
+
+          <label className='labelChat'>
+
             Chat name:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
+            <input type="text" value={this.state.chatName} onChange={this.chatNameChange} />
+            
           </label>
-          <label>
+
+          <label className='labelUser'>
+
             User name:
-            <input type="text" value={this.state.value2} onChange={this.handleChange} />
+            <input type="text" value={this.state.userName} onChange={this.userNameChange} />
+
           </label>
+
           <input type="submit" value="Send" />
+
         </form>
       );
     }
-  }
+}
 
 export default ChatForm;
--- src/pages/createChat.js
+++ src/pages/createChat.js
@@ -2,12 +2,16 @@ import React from 'react'
 import '../index.css'
 import ChatForm from '../ChatForms/chatForm'
 
-function CreateChat(){
+
+class CreateChat extends React.Component {
+
+    render(){
     return (
         <div className='createchat'>
             <ChatForm></ChatForm>
         </div>
     )
+    }
 }
 
 export default CreateChat;
